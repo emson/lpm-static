ecdd7da77874d785b13fe67c48f298029e8fbc61
Ben Emson <ben@emson.co.uk>
2011-04-13 22:32:41 +0100

    Added spring newletter landing page

--- src/helpers/menu_helper.rb
+++ src/helpers/menu_helper.rb
@@ -4,9 +4,12 @@ module MenuHelper
   
   def menu_item(page, title, html_class=nil)
     page_name = "#{page}.html"
-    return "<span class='active #{html_class}'>#{title}</span>" if "/#{page_name}" == current_page
-    html_class = (html_class.nil?) ? "" : "class='#{html_class}'" 
-    return "<a href='#{page_name}' #{html_class}>#{title}</a>"
+    if "/#{page_name}" == current_page 
+      return "<span class='active #{html_class}'>#{title}</span>"
+    else
+      html_class = (html_class.nil?) ? "" : "class='#{html_class}'" 
+      return "<a href='/#{page_name}' #{html_class}>#{title}</a>"
+    end
   end
   
 end
